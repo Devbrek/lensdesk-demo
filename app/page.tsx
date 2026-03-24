e826f5ab10c0c0835d45005673fa66f240349b24
Devbrek <brek.developpement@gmail.com>
2026-03-24 08:30:35 +0100

    modif page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
-      <main>Bonjour</main>
+      <main>
+        <div className="min-h-screen flex flex-col items-center justify-center  p-5">
+          <h1 className="text-5xl font-bold mb-8 text-white">
+            Bienvenue dans l&apos;application &quot;ABUZONE CHECKER&quot;
+          </h1>
+          <button
+            onClick={() => router.push("/login")}
+            className="w-full px-5 bg-white text-black rounded-full py-3 mt-2 font-semibold hover:bg-gray-200 transition uppercase"
+          >
+            Se login
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
