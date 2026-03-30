728f5dd06fa03eacad4204c1f7d54237c1d228bf
Devbrek <brek.developpement@gmail.com>
2026-03-30 12:00:10 +0200

    fix navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import { fa } from "zod/locales";
 
 const Navbar = () => {
   const router = useRouter();
@@ -15,70 +14,68 @@ const Navbar = () => {
 
   const handleLogout = () => {
     localStorage.removeItem("user");
-
     setOpen(false);
-
     router.replace("/");
   };
 
   return (
-    <nav className="flex py-2 w-screen justify-between px-10 absolute z-10 top-0 left-0 items-center  backdrop-blur-sm ">
-      {/* LOGO */}
-      <button
-        className="text-white flex gap-3"
-        onClick={() => handleNav("/dashboard")}
-      >
-        <img src="/icons/logo2.png" alt="logo" className="w-25" />
-      </button>
-
-      {/* MENU DESKTOP */}
-      <div className="hidden md:block bg-white/10 px-5 py-2 rounded-full border-2 border-white text-white uppercase font-bold">
-        <ul className="flex gap-5">
-          <li
-            onClick={() => handleNav("/dashboard")}
-            className="cursor-pointer"
-          >
-            Dashboard
-          </li>
-          <li
-            onClick={() => handleNav("/inventory")}
-            className="cursor-pointer"
-          >
-            Inventaire
-          </li>
-          <li
-            onClick={() => handleNav("/shootings")}
-            className="cursor-pointer"
-          >
-            Shootings
-          </li>
-          <li onClick={() => handleLogout()} className="cursor-pointer">
-            Se deconnecter
-          </li>
-        </ul>
-      </div>
-
-      {/* BOUTON A PROPOS */}
-      <div className="hidden md:block">
+    <>
+      {/* NAVBAR */}
+      <nav className="fixed top-0 left-0 z-40 w-screen flex py-2 justify-between px-10 items-center backdrop-blur-sm">
+        {/* LOGO */}
         <button
-          onClick={() => router.push("/apropos")}
-          className="bg-white/10 px-5 py-3 rounded-full border-2 border-white text-white uppercase font-bold"
+          className="text-white flex gap-3"
+          onClick={() => handleNav("/dashboard")}
         >
-          A propos
+          <img src="/icons/logo2.png" alt="logo" className="w-24" />
         </button>
-      </div>
 
-      {/* MOBILE BUTTON */}
-      <button
-        onClick={() => setOpen(true)}
-        className="md:hidden text-black  px-4 py-2"
-      >
-        <img src="/icons/menuW.svg" alt="menu" className="w-10" />
-      </button>
+        {/* MENU DESKTOP */}
+        <div className="hidden md:block bg-white/10 px-5 py-2 rounded-full border-2 border-white text-white uppercase font-bold">
+          <ul className="flex gap-5">
+            <li
+              onClick={() => handleNav("/dashboard")}
+              className="cursor-pointer"
+            >
+              Dashboard
+            </li>
+            <li
+              onClick={() => handleNav("/inventory")}
+              className="cursor-pointer"
+            >
+              Inventaire
+            </li>
+            <li
+              onClick={() => handleNav("/shootings")}
+              className="cursor-pointer"
+            >
+              Shootings
+            </li>
+            <li onClick={handleLogout} className="cursor-pointer">
+              Se deconnecter
+            </li>
+          </ul>
+        </div>
+
+        {/* A PROPOS */}
+        <div className="hidden md:block">
+          <button
+            onClick={() => router.push("/apropos")}
+            className="bg-white/10 px-5 py-3 rounded-full border-2 border-white text-white uppercase font-bold"
+          >
+            A propos
+          </button>
+        </div>
+
+        {/* BURGER */}
+        <button onClick={() => setOpen(true)} className="md:hidden">
+          <img src="/icons/menuW.svg" alt="menu" className="w-10" />
+        </button>
+      </nav>
 
       {/* OVERLAY */}
       <div
-        className={`fixed inset-0 bg-black/50 transition-opacity duration-300 ${
+        className={`fixed inset-0 z-40 bg-black/50 transition-opacity duration-300 ${
           open
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
@@ -86,44 +83,45 @@ const Navbar = () => {
         onClick={() => setOpen(false)}
       />
 
-      {/* MENU MOBILE SLIDE */}
+      {/* MOBILE MENU */}
       <div
-        className={`fixed top-0 left-0 h-screen   max-w-xs bg-sky-900  text-white p-6 py-12 flex flex-col gap-6 uppercase font-bold transform transition-transform duration-300 ease-in-out z-20
-        ${open ? "translate-x-0" : "-translate-x-full"}`}
+        className={`fixed top-0 left-0 z-50 h-screen w-72 bg-sky-900 text-white p-6 py-12 flex flex-col gap-6 uppercase font-bold transform transition-transform duration-300 ease-in-out ${
+          open ? "translate-x-0" : "-translate-x-full"
+        }`}
       >
-        <div className="flex justify-between mb-20">
-          <img src="/icons/logo2.png" alt="logo" className="w-1/2 " />
-          {/* BOUTON FERMER */}
-          <button
-            onClick={() => setOpen(false)}
-            className="self-end text-xl font-bold  my-auto "
-          >
+        {/* HEADER */}
+        <div className="flex justify-between items-center mb-16">
+          <img src="/icons/logo2.png" alt="logo" className="w-1/2" />
+
+          <button onClick={() => setOpen(false)} className="text-xl font-bold">
             ✕
           </button>
         </div>
 
-        {/* LIENS */}
-        <button className="uppercase" onClick={() => handleNav("/dashboard")}>
+        {/* LINKS */}
+        <button onClick={() => handleNav("/dashboard")} className="uppercase">
           Dashboard
         </button>
-        <button className="uppercase" onClick={() => handleNav("/shootings")}>
+        <button onClick={() => handleNav("/shootings")} className="uppercase">
           Shootings
         </button>
-        <button className="uppercase" onClick={() => handleNav("/inventory")}>
+        <button onClick={() => handleNav("/inventory")} className="uppercase">
           Inventaire
         </button>
-        <button className="uppercase" onClick={() => handleNav("/apropos")}>
+        <button onClick={() => handleNav("/apropos")} className="uppercase">
           A propos
         </button>
+
+        {/* LOGOUT */}
         <button
-          className="uppercase mt-20 items-center justify-center flex flex-col gap-3"
-          onClick={() => handleLogout()}
+          onClick={handleLogout}
+          className="mt-auto flex flex-col items-center gap-3 uppercase"
         >
-          <img src="/icons/logout.svg" alt="logout" className="w-12" />
+          <img src="/icons/logout.svg" alt="logout" className="w-10" />
           Se deconnecter
         </button>
       </div>
-    </nav>
+    </>
   );
 };
 
